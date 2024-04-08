8efd8d35c392965720d02832503a95a32d6deac0
Daniel | Narterh <48166091+Mr-Deee@users.noreply.github.com>
2024-04-08 14:01:29 +0000

    continiouspage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
 import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import  HomePage from '../src/Pages/home';
+import  ContinuousPage  from '../src/Pages/continuouspage';
 
 function App() {
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const sections = document.querySelectorAll('.section');
+      sections.forEach(section => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const sectionBottom = section.getBoundingClientRect().bottom;
+
+        // Check if the section is partially visible
+        if (sectionTop < window.innerHeight && sectionBottom >= 0) {
+          section.classList.add('visible');
+        } else {
+          section.classList.remove('visible');
+        }
+      });
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const scrollToSection = (id) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav  className="navbar">
+        <ul className="navbar-nav" >
+          <li><button onClick={() => scrollToSection('about')}>About</button></li>
+          <li><button onClick={() => scrollToSection('contact')}>Contact Us</button></li>
+        </ul>
+      </nav>
+           <HomePage />  
+
+      <ContinuousPage />
+
+     
     </div>
   );
 }
---
+++ src/Pages/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './pages.css';
+
+const HomePage = () => {
+  return (
+    <div className="container">
+      <h1 className="title">Dossier</h1>
+      <p>Welcome to Dossier, your personal information hub.</p>
+    </div>
+  );
+}
+
+export default HomePage;
